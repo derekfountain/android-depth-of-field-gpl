a1125269b13bf05015050d11993a8c32504cfd23
derekfountain@gmail.com <derekfountain@gmail.com@21fe6e9e-2bf9-c609-889d-47511dfcf958>
2011-08-13 19:44:23 +0000

    Commit v0.92b

--- trunk/src/org/derekfountain/dofc/DepthOfFieldCalc.java
+++ trunk/src/org/derekfountain/dofc/DepthOfFieldCalc.java
@@ -7,6 +7,7 @@ import org.derekfountain.dofc.m.Body;
 import org.derekfountain.dofc.m.Lens;
 import org.derekfountain.dofc.m.Range;
 import org.derekfountain.dofc.v.MVCView;
+import org.derekfountain.dofc.v.RotatingSpinner;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -58,6 +59,30 @@ public class DepthOfFieldCalc extends TabActivity {
 	}
 
 	/**
+	 * This is a workaround for issue
+	 * https://code.google.com/p/android-depth-of-field-gpl/issues/detail?id=1&can=1
+	 * If you create a tab in the Android TabWidget, then delete it
+	 * by removing all tabs, then recreate it, then switch to it - and
+	 * that's a sequence that's perfectly possible with this app - then
+	 * you get a crash deep in the tab widget. The workaround is to
+	 * store in this array a list of the previously deleted tab names
+	 * so I can tell if the user recreates one, and so something about
+	 * it.
+	 */
+	protected static ArrayList<String> previouslyDeletedTabs   = new ArrayList<String>();
+	
+	/**
+	 * Workaround for issue
+	 *  http://code.google.com/p/android-depth-of-field-gpl/issues/detail?id=2
+	 * I've created a Spinner class which extends the regular Android
+	 * spinner, only giving it the ability to withstand the phone being
+	 * rotated while the spinner's list dialog is dropped.
+	 */
+    public RotatingSpinner bodySpinner  = null;
+    public RotatingSpinner lensSpinner  = null;
+    public RotatingSpinner rangeSpinner = null;
+	
+	/**
 	 * The main "wrapping" Activity start point.
 	 * <p>
 	 * When this starts up there are no Pages running. They will
@@ -81,6 +106,10 @@ public class DepthOfFieldCalc extends TabActivity {
         	applicationState.restoreFromBundle(savedInstanceState);
         else
         	applicationState.restoreFromDefaults( this );
+        
+        // Start off this list as empty
+        //
+        previouslyDeletedTabs = new ArrayList<String>();
         
         rebuildTabs();       
     }
@@ -177,8 +206,21 @@ public class DepthOfFieldCalc extends TabActivity {
     public void onPause()
     {
     	super.onPause();
+    	
+    	// If any of the new tab dialog's spinners are dropped,
+    	// dismiss them.
+    	//
+    	if ( bodySpinner != null ) {
+    		bodySpinner.dismissPoppedUpList();
+    	}
+    	if ( lensSpinner != null ) {
+    		lensSpinner.dismissPoppedUpList();
+    	}
+    	if ( rangeSpinner != null ) {
+    		rangeSpinner.dismissPoppedUpList();
+    	}
     }
-    
+
     /**
      * This is called when the menu is created, at the start.
      */
@@ -232,15 +274,15 @@ public class DepthOfFieldCalc extends TabActivity {
     	}
 
     }
-    
+
     /**
      * This is called when one of the application's dialogs is requested.
      */
     @Override
     protected Dialog onCreateDialog( int id )
     {
-    	AlertDialog         dialog   = null;
     	Resources           res      = getResources();
+    	AlertDialog         dialog   = null;
 
     	AlertDialog.Builder builder  = new AlertDialog.Builder( this );
     	LayoutInflater      inflater = getLayoutInflater();     // Activity window inflater, not the app one
@@ -277,8 +319,9 @@ public class DepthOfFieldCalc extends TabActivity {
     				lastUsedBodyPosition = i;
     			i++;
     		}
-    		((Spinner)layout.findViewById(R.id.BodySpinner)).setAdapter(bodiesAdapter);
-    		
+    		bodySpinner = ((RotatingSpinner)layout.findViewById(R.id.BodySpinner));
+    		bodySpinner.setAdapter(bodiesAdapter);
+  
     		// Populate an adapter for the lens spinner, taking a note of position of the last used value    		
     		//    		
     		ArrayAdapter<CharSequence> lensAdapter = new ArrayAdapter<CharSequence>( this, android.R.layout.simple_spinner_item );
@@ -292,7 +335,8 @@ public class DepthOfFieldCalc extends TabActivity {
     				lastUsedLensPosition = i;
     			i++;
     		}
-    		((Spinner)layout.findViewById(R.id.LensSpinner)).setAdapter(lensAdapter);
+    		lensSpinner = ((RotatingSpinner)layout.findViewById(R.id.LensSpinner));
+    		lensSpinner.setAdapter(lensAdapter);
 
     		// Populate an adapter for the range spinner, taking a note of position of the last used value    		
     		//    		
@@ -307,13 +351,14 @@ public class DepthOfFieldCalc extends TabActivity {
     				lastUsedRangePosition = i;
     			i++;
     		}
-    		((Spinner)layout.findViewById(R.id.RangeSpinner)).setAdapter(rangeAdapter);
+    		rangeSpinner = ((RotatingSpinner)layout.findViewById(R.id.RangeSpinner));
+    		rangeSpinner.setAdapter(rangeAdapter);
     		
     		// Prime the defaults, as used last time the dialog was accessed
     		//
-    		((Spinner)layout.findViewById(R.id.BodySpinner)).setSelection(lastUsedBodyPosition);
-    		((Spinner)layout.findViewById(R.id.LensSpinner)).setSelection(lastUsedLensPosition);
-    		((Spinner)layout.findViewById(R.id.RangeSpinner)).setSelection(lastUsedRangePosition);
+    		bodySpinner.setSelection(lastUsedBodyPosition);
+    		lensSpinner.setSelection(lastUsedLensPosition);
+    		rangeSpinner.setSelection(lastUsedRangePosition);
     		
     		// The OK button is linked to a bit of code which adds a new tab to the main activity's tab host.
     		//
@@ -443,6 +488,12 @@ public class DepthOfFieldCalc extends TabActivity {
 	    	applicationState.getKnownPages().remove(tagToDelete);
 	    	applicationState.getKnownTabs().remove(tagToDelete);
 	    	applicationState.setActivePage(null);
+	        
+	    	// Add the name of the deleted tab to the list, so I can tell
+	    	// if the user reuses it. See comments at the declaration of
+	    	// previouslyDeletedTabs
+	    	//
+	    	previouslyDeletedTabs.add(tagToDelete);
 
 	    	// This appears to be necessary, otherwise the app hits a NPE in the tab widget
 	    	//
@@ -507,6 +558,14 @@ public class DepthOfFieldCalc extends TabActivity {
 				warningToast.show();						
 			}
 			else {
+				
+				// If the user has reused a tab name that they've previously
+				// deleted, sneakliy change it by prepending a space to it.
+				// See the comments at the declaration of previouslyDeletedTabs
+				// for the explanation of why this nonsense is necessary.
+				//
+				while ( previouslyDeletedTabs.contains(tabName) )
+					tabName = " "+tabName;
 				
 				// Everything is OK - create a new tab
 				//
